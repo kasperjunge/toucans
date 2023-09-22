38a8e0f06d194ebd22b0f79e31c3d63455cb2175
Kasper Junge <kasperjunge@MacBook-Pro-tilhrende-Kasper.local>
2023-09-22 10:58:50 +0200

    .

---
+++ toucans/core.py
@@ -0,0 +1,14 @@
+class Prompt:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return "Prompt Object"
+
+
+class ChatFlow:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return "ChatFlow Object"
